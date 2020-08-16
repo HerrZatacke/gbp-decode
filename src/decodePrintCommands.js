9cea8f624daa27170c9e292b89b412d1ca5d4331
HerrZatacke <zatacke@gmail.com>
2020-08-16 11:36:20 +0200

    add palette decoding

--- src/decodePrintCommands.js
+++ src/decodePrintCommands.js
@@ -1,5 +1,19 @@
 const { COMMAND_PRINT } = require("./constants");
 
+/*
+ The palette table is one-byte data, consisting of four two-bit values.
+ The highest two bits in a palette byte represent the brightness of dots with index 0.
+ The next two bits represent those of index 1, the next, index 2, and the lowest two bits, index 3.
+ */
+const paletteData = (paletteRaw) => {
+  return [
+    (paletteRaw >> 6) & 0x3,
+    (paletteRaw >> 4) & 0x3,
+    (paletteRaw >> 2) & 0x3,
+    (paletteRaw >> 0) & 0x3,
+  ];
+};
+
 const decodePrintCommands = (packets) => {
   return packets
     .map((packet) => {
@@ -11,6 +25,7 @@ const decodePrintCommands = (packets) => {
             marginUpper: packet.data[1] >> 4,
             marginLower: packet.data[1] & 0xf,
             palette: packet.data[2],
+            paletteData: paletteData(packet.data[2]),
           }
         }
       }
