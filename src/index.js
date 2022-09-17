75f3bfe36b6c461b19ac56e339462795c8c1cc71
Andreas Hahn <zatacke@gmail.com>
2022-09-17 12:12:20 +0200

    Add test process for reduced output stream data

--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ const path = require('path');
 
 const toByteArray = require('./toByteArray');
 const parsePackets = require('./parsePackets');
+const parseReducedPackets = require('./parseReducedPackets');
+const inflateTransferPackages = require('./inflateTransferPackages');
 const getImageDataStream = require('./getImageDataStream');
 const decompressDataStream = require('./decompressDataStream');
 const decodePrintCommands = require('./decodePrintCommands');
 const harmonizePalettes = require('./harmonizePalettes');
 const transformToClassic = require('./transformToClassic');
+const completeFrame = require('./completeFrame');
 
 const fileName = 'white.txt';
 const filePath = path.join(process.cwd(), fileName)
@@ -29,6 +32,35 @@ fs.readFile(filePath, { encoding: 'utf8' }, (error, fileContents) => {
       images.forEach((image, index) => {
         image.unshift('!{"command":"INIT"}');
         fs.writeFileSync(path.join(process.cwd(), `out_${index}.txt`), image.join('\n'), {encoding: 'utf8'});
+      });
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+});
+
+const picoFileName = 'pico.txt';
+const picoFilePath = path.join(process.cwd(), picoFileName)
+
+fs.readFile(picoFilePath, { encoding: 'utf8' }, (error, fileContents) => {
+  if (error) {
+    console.error(error.message);
+    process.exit(-1);
+  }
+
+  toByteArray(fileContents)
+    .then(parseReducedPackets)
+    .then(inflateTransferPackages)
+    .then(getImageDataStream)
+    .then(decompressDataStream)
+    .then(decodePrintCommands)
+    .then(harmonizePalettes)
+    .then(transformToClassic)
+    .then(completeFrame)
+    .then((images) => {
+      images.forEach((image, index) => {
+        image.unshift('!{"command":"INIT"}');
+        fs.writeFileSync(path.join(process.cwd(), `out_pico_${index}.txt`), image.join('\n'), {encoding: 'utf8'});
       });
     })
     .catch((error) => {
